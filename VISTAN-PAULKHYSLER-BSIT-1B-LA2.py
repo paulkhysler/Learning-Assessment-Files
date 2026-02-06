5f853ba85544a74ea5d25b8e33756c4325cf31d6
Paul Khysler Vistan <paulkhyslervistan@gmail.com>
2026-02-07 00:50:09 +0800

    Add files via upload

---
+++ VISTAN-PAULKHYSLER-BSIT-1B-LA2.py
@@ -0,0 +1,12 @@
+# LA#2: Comparison Operators
+# Get the two integer inputs from user
+x = int(input("Enter the value for x: "))
+y = int(input("Enter the value for y: "))
+
+# Result of all comparison operations using str()
+print("\n" + str(x) + " > " + str(y) + " = " + str(x > y))
+print(str(x) + " >= " + str(y) + " = " + str(x >= y))
+print(str(x) + " < " + str(y) + " = " + str(x < y))
+print(str(x) + " <= " + str(y) + " = " + str(x <= y))
+print(str(x) + " == " + str(y) + " = " + str(x == y))
+print(str(x) + " != " + str(y) + " = " + str(x != y))
