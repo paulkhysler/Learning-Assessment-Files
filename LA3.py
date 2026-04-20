da4b94c26ac24e6d38a7402aaca866370fc682d3
Paul Khysler Vistan <paulkhyslervistan@gmail.com>
2026-04-20 16:13:15 +0800

    Create LA3.py

---
+++ LA3.py
@@ -0,0 +1,10 @@
+# Temperature Checker Program
+
+temperature = int(input("Enter the current temperature: "))
+
+if temperature > 30:
+    print("Hot day! Stay hydrated.")
+elif temperature < 10:
+    print("Cold day! Keep your body warm!")
+else:
+    print("Pleasant weather!")
