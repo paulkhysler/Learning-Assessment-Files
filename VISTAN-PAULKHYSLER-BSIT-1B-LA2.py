e71ad09e876eeb0b7ad2adc0560092bb28bf41c2
Paul Khysler Vistan <paulkhyslervistan@gmail.com>
2026-04-25 12:42:53 +0800

    Refactor comparison operator script for clarity
    Updated variable names and improved output formatting.

--- VISTAN-PAULKHYSLER-BSIT-1B-LA2.py
+++ VISTAN-PAULKHYSLER-BSIT-1B-LA2.py
@@ -1,12 +1,13 @@
-# LA#2: Comparison Operators
-# Get the two integer inputs from user
-x = int(input("Enter the value for x: "))
-y = int(input("Enter the value for y: "))
+# LA#2 - Comparison Operators
+# Get two integer inputs from user
+value_1 = int(input("Enter value for x: "))
+value_2 = int(input("Enter value for y: "))
 
-# Result of all comparison operations using str()
-print("\n" + str(x) + " > " + str(y) + " = " + str(x > y))
-print(str(x) + " >= " + str(y) + " = " + str(x >= y))
-print(str(x) + " < " + str(y) + " = " + str(x < y))
-print(str(x) + " <= " + str(y) + " = " + str(x <= y))
-print(str(x) + " == " + str(y) + " = " + str(x == y))
-print(str(x) + " != " + str(y) + " = " + str(x != y))
+# Result of all comparison operations
+print("\nResult:")
+print(f"{value_1} > {value_2} = {value_1 > value_2}")
+print(f"{value_1} >= {value_2} = {value_1 >= value_2}")
+print(f"{value_1} < {value_2} = {value_1 < value_2}")
+print(f"{value_1} <= {value_2} = {value_1 <= value_2}")
+print(f"{value_1} == {value_2} = {value_1 == value_2}")
+print(f"{value_1} != {value_2} = {value_1 != value_2}")
